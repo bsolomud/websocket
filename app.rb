edfdc7857d500281addcbc31e77d6922ad323083
Bodya Solomud <kilbioas@gmail.com>
2015-07-13 18:45:01 +0300

    Send messages to each user

--- app.rb
+++ app.rb
@@ -1,27 +1,38 @@
 require 'eventmachine'
 require 'em-websocket'
 
-
 EM.run {
+  @sockets = []
   EM::WebSocket.start({
-                          host: "localhost",
+                          host: 'localhost',
                           port: 8187,
                           debug: true
 
                       }) do |ws|
     ws.onopen {
-      puts "Socket connection start"
-
-      ws.send "You are now connected"
+      @sockets << ws
+      # puts "Socket connection is started"
+      # ws.send @chat_text
     }
 
     ws.onmessage { |msg|
-      ws.send "Pong #{msg}"
+      @sockets.each do |socket|
+        socket.send msg
+      end
     }
 
-    ws.onclose {
-      puts "Socket connection closed"
-      ws.close
-    }
+    ws.onclose do
+      puts "Socket connection is closed"
+      @sockets.delete ws
+    end
+
+    # def ws_open(sock)
+    #   @sockets << sock
+    #   p "SOCKETSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS #{@sockets}"
+    # end
+    #
+    # def ws_message
+    #
+    # end
   end
 }
