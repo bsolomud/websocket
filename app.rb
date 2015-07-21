acd59bef5059bd06e22a26f2c7449f778a72f415
Stas Liudera <StasLiudera@gmail.com>
2015-07-21 19:02:35 +0300

    direct messages

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'em-websocket'
 require 'amqp'
+require 'json'
 
 HOST = 'localhost'
 
-def deconstruct!(message)
-  @key = message.slice 0, message.index(':')
-  @value = message.slice message.index(':') + 1, message.size - 1
+class Message
+  attr_reader :sender, :receiver, :text
+
+  def initialize(sender, receiver, text)
+    @sender = sender
+    @receiver = receiver
+    @text = text
+  end
+
+  def to_json
+    { sender: @sender, receiver: @receiver, text: @text }.to_json
+  end
 end
 
 def generate_queue_name(value)
-  value.tr(' ', '_').tr('-', '_').downcase
+  value.tr(' -_', '').downcase
 end
 
 EM.run {
@@ -25,44 +35,72 @@ EM.run {
     ws.onopen {
       @web_sockets << ws
 
-      puts "Client is connected"
+      puts 'Client is connected'
 
-      @connection ||= AMQP.connect host: HOST
-      @channel ||= AMQP::Channel.new @connection
+      @connection = AMQP.connect host: HOST
+      @channel = AMQP::Channel.new @connection
+      @exchange = @channel.topic 'demo'
 
-      # ws.send @chat_text
+      @queue_is_subscribed = false
     }
 
     ws.onmessage do |msg|
+
+      msg = JSON.parse msg
+
       p "======================= MSG: #{msg}"
 
-      deconstruct! msg
+      # @exchange ||= if @channel_type == 'direct'
+      #                 @channel.default_exchange
+      #               elsif @channel_type = 'fanout'
+      #                 @channel.fanout 'centre.ua'
+      #               end
 
-      case @key
-        when 'user_name'
-          @user_name = @value
-          @users << @user_name unless @users.include? @user_name
-          ws.send "users:#{@users.join ', '}"
-        when 'to'
-          @queue_name = generate_queue_name @value
-        when 'message'
-          # AMQP setup
+      if msg['sender']
+        @queue_name = generate_queue_name msg['sender']
+
+        unless @queue_is_subscribed
+          # queue = if @channel_type == 'direct'
+          #           @channel.queue(@queue_name)
+          #         elsif @channel_type == 'fanout'
+          #           @channel.queue(@queue_name).bind(@exchange)
+          #         end
           @queue = @channel.queue @queue_name
-          @exchange ||= @channel.direct ''
+
           @queue.subscribe do |payload|
-            ws.send "message:#{payload}"
+            ws.send payload
           end
 
-          @exchange.publish "user_name:#{@user_name}", routing_key: @queue_name
-          @exchange.publish "message:#{@message}", routing_key: @queue_name
-          @messages << @value
+          @queue_is_subscribed = true
+        end
+
+        @users << msg['sender'] unless @users.include? msg['sender']
+
+        users = { users: @users }
+
+        @web_sockets.each do |web_socket|
+          web_socket.send users.to_json
+        end
+      end
+
+      @receiver_routing_key = msg['receiver'] if msg['receiver']
+
+      @messages << msg['text'] if msg['text']
+
+      if msg['sender'] && msg['receiver'] && msg['text']
+        @msg = Message.new msg['sender'], msg['receiver'], msg['text']
+
+        @exchange.publish @msg.to_json, routing_key: @queue_name
+        # if @channel_type == 'direct'
+        @exchange.publish @msg.to_json, routing_key: @receiver_routing_key
+        # end
       end
     end
 
     ws.onclose do
-      puts "Client is disconnected"
-      @connection.close
+      puts 'Client is disconnected'
       @web_sockets.delete ws
+      @connection.close
     end
   end
 }
